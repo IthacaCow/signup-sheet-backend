f42d44e75925e8a139f60a7f9406157f1b2cf54f
IthacaCow <cba840910@gmail.com>
2015-07-06 06:00:13 +0800

    Add new routes into routes/sign_up.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var bodyParser   = require('body-parser');
+var sign_up      = require('./routes/sign_up');
+var mongoose = require('mongoose');
 
 var app = express();
 
@@ -22,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', sign_up);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/sign_up.js
@@ -0,0 +1,17 @@
+
+exports.add_record = function (req, res) {
+	
+};
+
+exports.delete_record = function (req, res) {
+	
+};
+
+exports.get_specific_record = function (req, res) {
+	
+};
+
+exports.get_all_records = function (req, res) {
+	
+};
+
